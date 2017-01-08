bd32aafee469b63d2f0f5bb6d8d44f30e16f432b
Kade Kenrick <KDiaCodes@users.noreply.github.com>
2017-01-08 20:34:52 +0100

    added member welcomer

--- bot.lua
+++ bot.lua
@@ -4,7 +4,9 @@ local client = discordia.Client()
 client:on('ready', function()
     print('Logged in as '.. client.user.username)
 end)
-
+client:on("memberJoin", function(member)
+       message.channel:sendMessage('member.name has joined')
+       member:sendMessage("welcome to the server") end)
 client:on('messageCreate', function(message)
     if message.content == '?/ping' then
         message.channel:sendMessage('pong')
